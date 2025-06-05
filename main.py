7617812f49bd42f1118646f15531ee410e894efc
Brunomri <19270033+brunomri@users.noreply.github.com>
2025-06-05 14:12:16 -0300

    Update classification prompt and write result to file.

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from rag.embeddings import get_vector_store
 from rag.rag_chain import query_rag
 
+def classify(result_path="results/classification/"):
+    """Classify product reviews into topics and write output to file"""
+    question = """
+    Please extract the topics mentioned in the e-commerce product reviews provided. A few topic examples are: product quality, 
+    delivery, customer service, packaging and usage. For each topic, add how many reviews belong to it. 
+    A review can belong to multiple topics. Present the result in a table where each row has a pair of topics and review count, 
+    write the result in portuguese.
+    """
+    query_rag(question, get_vector_store(), "classification", result_path)
+
 if __name__ == "__main__":
     # TODO: In case the index doesn't exist, create it by calling
     #  index_documents(load_data()) instead of get_vector_store()
-    question = "Please extract the topics mentioned in the review texts."
-    result = query_rag(question, get_vector_store())
-    print(result)
+    classify()
+
--- rag/rag_chain.py
+++ rag/rag_chain.py
@@ -1,17 +1,29 @@
+from datetime import datetime
+from pathlib import Path
+
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnablePassthrough
 from langchain_ollama import ChatOllama
 
 
-def query_rag(question, vector_store):
+def query_rag(question, vector_store, result_type, result_path):
     """Queries the RAG chain and prints the response."""
     rag_chain = create_rag_chain(vector_store)
     print("\nQuerying RAG chain...")
-    print(f"Question: {question}")
+    print(f"\nQuestion: {question}")
     response = rag_chain.invoke(question)
-    print("\nResponse:")
-    print(response)
+    print(f"\nResponse: {response}")
+
+    current_time = datetime.now().isoformat().split("T")
+    file_name = f"{result_type}_{current_time[0]}_{current_time[1]}.txt"
+
+    path = Path(result_path)
+    path.mkdir(parents=True, exist_ok=True)
+
+    with open(f"{result_path}{file_name}", "w") as result_file:
+        result_file.write(f"Question:\n{question}\nResponse:\n{response}")
+
 
 def create_rag_chain(vector_store):
     """Creates the RAG chain."""
@@ -32,7 +44,8 @@ def initialize_llm(model_name="qwen3:8b", temperature=0, context_window=8192):
     print(f"Initialized ChatOllama with model: {model_name}, temperature: {temperature}, context window: {context_window}")
     return llm
 
-def create_retriever(vector_store, search_type="similarity", top_chunks=3):
+# TODO: Review the need of top_chunks parameter
+def create_retriever(vector_store, search_type="similarity", top_chunks=20):
     retriever = vector_store.as_retriever(
         search_type=search_type,
         search_kwargs={'k':top_chunks}
