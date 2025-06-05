d521c1ede56b3584cdfe89cba937fdea89ea6cfa
Brunomri <19270033+brunomri@users.noreply.github.com>
2025-06-05 14:48:05 -0300

    Add summarization by product.

--- main.py
+++ main.py
@@ -6,13 +6,23 @@ def classify(result_path="results/classification/"):
     question = """
     Please extract the topics mentioned in the e-commerce product reviews provided. A few topic examples are: product quality, 
     delivery, customer service, packaging and usage. For each topic, add how many reviews belong to it. 
-    A review can belong to multiple topics. Present the result in a table where each row has a pair of topics and review count, 
+    A review can belong to multiple topics. Present the result in a table where each row has the topic and its review count, 
     write the result in portuguese.
     """
     query_rag(question, get_vector_store(), "classification", result_path)
+
+def summarize_by_product(result_path="results/summarization/product/"):
+    """Summarize product reviews by product, brand or category and write output to file"""
+    question = """
+    Please summarize the reviews by field product_id. 
+    Present the result in a table where each row has the product ID, product name and summary, write the result in portuguese.
+    Ignore reviews where the field product_name is blank.
+    """
+    query_rag(question, get_vector_store(), "summary_by_product", result_path)
 
 if __name__ == "__main__":
     # TODO: In case the index doesn't exist, create it by calling
     #  index_documents(load_data()) instead of get_vector_store()
     classify()
+    summarize_by_product()
 
