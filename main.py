d9ba7baab9d4e0fdda1a78832861044160ae69b8
Brunomri <19270033+brunomri@users.noreply.github.com>
2025-06-05 15:05:37 -0300

    Add summarization by brand.

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def classify(result_path="results/classification/"):
     query_rag(question, get_vector_store(), "classification", result_path)
 
 def summarize_by_product(result_path="results/summarization/product/"):
-    """Summarize product reviews by product, brand or category and write output to file"""
+    """Summarize reviews by product and write output to file"""
     question = """
     Please summarize the reviews by field product_id. 
     Present the result in a table where each row has the product ID, product name and summary, write the result in portuguese.
@@ -20,9 +20,19 @@ def summarize_by_product(result_path="results/summarization/product/"):
     """
     query_rag(question, get_vector_store(), "summary_by_product", result_path)
 
+def summarize_by_brand(result_path="results/summarization/brand/"):
+    """Summarize reviews by brand and write output to file"""
+    question = """
+    Please summarize the reviews of all products from a certain brand using field product_brand.
+    Present the result in a table where each row has the product brand and summary, write the result in portuguese.
+    Ignore reviews where the field product_brand is blank.
+    """
+    query_rag(question, get_vector_store(), "summary_by_brand", result_path)
+
 if __name__ == "__main__":
     # TODO: In case the index doesn't exist, create it by calling
     #  index_documents(load_data()) instead of get_vector_store()
     classify()
     summarize_by_product()
+    summarize_by_brand()
 
